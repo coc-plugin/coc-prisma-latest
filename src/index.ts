5e7a2eebb06657d3aaa713eef2040c690c0e5a72
chuyanlong <a1008611abcd@126.com>
2025-10-23 17:23:02 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+// Most of the parts are taken directly from https://github.com/prisma/vscode/blob/master/packages/vscode/src/extension.ts
+// Imports are transformed so that they can work with CoC
+import {
+  ExtensionContext,
+  LanguageClient,
+  TransportKind,
+  ServerOptions,
+  LanguageClientOptions,
+  commands,
+  window,
+} from "coc.nvim";
+
+function createLanguageServer(
+  serverOptions: ServerOptions,
+  clientOptions: LanguageClientOptions
+) {
+  return new LanguageClient(
+    "prisma",
+    "Prisma Language Server",
+    serverOptions,
+    clientOptions
+  );
+}
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const serverModule = require.resolve("@prisma/language-server/dist/bin");
+
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = {
+    execArgv: ["--nolazy", "--inspect=6009"],
+    env: { DEBUG: true },
+  };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for prisma documents
+    documentSelector: [{ scheme: "file", language: "prisma" }],
+  };
+
+  // Create the language client
+  let client = createLanguageServer(serverOptions, clientOptions);
+
+  // Start the client. This will also launch the server
+  let disposable = client.start();
+
+  context.subscriptions.push(disposable);
+
+  context.subscriptions.push(
+    commands.registerCommand("coc-prisma.restartLanguageServer", async () => {
+      await client.stop();
+      client = createLanguageServer(serverOptions, clientOptions);
+      context.subscriptions.push(client.start());
+      await client.onReady();
+      window.showInformationMessage("Prisma language server restarted.");
+    })
+  );
+}
